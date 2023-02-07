51310d02b0202dfbc95f7b621349baa1dc68b453
Wiktor Zielinski <wiktorze@gmail.com>
2023-02-07 20:32:56 +0100

    more irrelevant messages

--- maketable.sh
+++ maketable.sh
@@ -3,3 +3,4 @@ echo hello bro, will make a contigenct table now!
 cd ~/gasprices
 cut -d ';' -f 5 carburants.csv | tr [:lower:] [:upper:] | sort | uniq -c | sort
 #save and exit
+echo program ran successfully!
