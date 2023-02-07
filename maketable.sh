9782238ab68fe00ae845ffa4c29526cf8902f98d
Wiktor Zielinski <wiktorze@gmail.com>
2023-02-07 20:16:53 +0100

    now the script greets you

--- maketable.sh
+++ maketable.sh
@@ -1,3 +1,5 @@
+echo third change to see if it works
+echo hello bro, will make a contigenct table now!
 cd ~/gasprices
 cut -d ';' -f 5 carburants.csv | tr [:lower:] [:upper:] | sort | uniq -c | sort
 #save and exit
